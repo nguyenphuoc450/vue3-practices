b63d1c5b9d7d4a1791c3ff9eb1453887c5ba1b8a
Nguyễn Phước <nguyenphuoc04052000@gmail.com>
2023-11-22 08:58:31 +0700

    chore: add node padding in VirtualScrollBasic.vue

--- src/components/virtual-scroll/VirtualScrollBasic.vue
+++ src/components/virtual-scroll/VirtualScrollBasic.vue
@@ -4,14 +4,14 @@ import VirtualScrollCard from './VirtualScrollCard.vue'
 
 const contentContainer = ref()
 const contentHeight = ref(0)
-const viewPortHeight = ref(560)
+const viewPortHeight = ref(400)
 const itemHeight = ref(56) // height 40 + 16 gap
 const dummyData = ref<any>([])
 const visibleItems = ref<any>([])
 const offsetY = ref(0)
 
 const getDummyData = () => {
-  dummyData.value = new Array(10000)?.fill(null)?.map((_: null, index: number) => ({ id: index, name: 'Lorem' + index }))
+  dummyData.value = new Array(10000)?.fill(null)?.map((_: null, index: number) => ({ id: index, name: 'Lorem ' + index }))
   contentHeight.value = dummyData.value.length * itemHeight.value
   const totalVisible = Math.ceil(viewPortHeight.value / itemHeight.value)
   visibleItems.value = dummyData.value.slice(0, totalVisible)
@@ -20,16 +20,17 @@ getDummyData()
 
 const handleScroll = (e: Event) => {
   const scrollTop = (e.target as HTMLElement)?.scrollTop
+  const nodePadding = 4
 
-  const startNode = Math.floor(scrollTop / itemHeight.value)
-  const totalVisible = Math.ceil(viewPortHeight.value / itemHeight.value)
+  const startNode = Math.max(0, Math.floor(scrollTop / itemHeight.value) - nodePadding)
+  const totalVisible = Math.ceil(viewPortHeight.value / itemHeight.value + 2 * nodePadding)
 
   offsetY.value = startNode * itemHeight.value
   const newData = dummyData.value.slice(startNode, startNode + totalVisible)
 
   visibleItems.value = newData.map((item: any, index: number) => ({
     id: startNode + index,
-    name: 'Lorem' + Number(startNode + index)
+    name: 'Lorem ' + Number(startNode + index)
   }))
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import VirtualScrollBasic from '~/components/virtual-scroll/VirtualScrollBasic.v
 <template>
   <div class="page">
     <div class="container mx-auto p-4">
-      <ul class="list-disc">
+      <ul class="pl-6 list-disc">
         <li>
           <RouterLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
                md:hover:text-blue-700 md:p-0" to="/virtual-scroll">
